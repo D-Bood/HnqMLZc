638a17d19af3ceb5b0889539b73e5250d52c2a0f
D-Bood <dbood2012@gmail.com>
2024-08-31 08:59:38 +0800

    del

--- web/src/components/MemoContent/Code.tsx
+++
@@ -1,9 +0,0 @@
-interface Props {
-  content: string;
-}
-
-const Code: React.FC<Props> = ({ content }: Props) => {
-  return <code className="text-sm">`{content}`</code>;
-};
-
-export default Code;
